8e5b776a79a4b7ba7dd28d576dd07fa4499269af
Jamie Falconer <mrfluffyoven@gmail.com>
2025-04-07 08:55:27 +0100

    Add legacy battery services
    Add support for legacy battery services, this will hopefully correct show the correct remaining capacity of your battery while in recovery
    
    Signed-off-by: Jamie Falconer <mrfluffyoven@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,6 +144,8 @@ TW_NO_BIND_SYSTEM := true
 TW_HAS_DOWNLOAD_MODE := true 
 TW_INCLUDE_LPTOOLS := true
 TW_INCLUDE_REPACKTOOLS := true
+TW_USE_LEGACY_BATTERY_SERVICES := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone31/temp"
 
 # USB 
